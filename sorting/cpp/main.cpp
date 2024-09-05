60fc48696b886dd9e48f1cf89658003b6ad1c9ea
Jagan-Mathematics <64853317+jagan-mathematics@users.noreply.github.com>
2024-09-05 13:01:28 +0530

    Update main.cpp

--- sorting/cpp/main.cpp
+++ sorting/cpp/main.cpp
@@ -10,6 +10,27 @@ void printArray(int arr[], int n)
 }
 
 
+template <typename Func, typename T>
+static void measureTime(Func sortFunction, std::vector<T>& array, const std::string& sortName) {
+    auto start = std::chrono::high_resolution_clock::now();
+    sortFunction(array);
+    auto stop = std::chrono::high_resolution_clock::now();
+
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << sortName << " took " << duration.count() << " microseconds." << std::endl;
+}
+
+
+template<typename S>
+void printArray(const std::vector<S>& array) {
+    for (const S& element : array) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;  // Newline after printing the array
+}
+
+
 int main(){
     int array[] = {0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 40, 4 , 1, 40, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4, 0, 4 , 1, 4};
     const int n = sizeof(array) / sizeof(array[0]);
@@ -21,4 +42,29 @@ int main(){
 
 //    Sorters<int, 4>::insertSort(array);
     printArray(array, n);
+
+
+
+    // BasicSorting algo testing
+    std::vector<int> arr1 = {64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11, 64, 25, 12, 22, 11};
+    std::vector<int> arr2 = arr1;
+    std::vector<int> arr3 = arr1;
+
+    std::cout << "Original array: ";
+    printArray<int>(arr1);
+
+
+    measureTime(basicSorting<int>::selectSort, arr1, "Selection Sort");
+    std::cout << "Sorted array (Selection Sort): ";
+    printArray<int>(arr1);
+
+
+    // Measure time for Bubble Sort (bubble sort will takes more time compared to all other because of its continuous swapping)
+    measureTime(basicSorting<int>::bubbleSort, arr3, "Bubble Sort");
+    std::cout << "Sorted array (Bubble Sort): ";
+    printArray<int>(arr3);
+
+    measureTime(basicSorting<int>::insertSort, arr2, "Insertion Sort");
+    std::cout << "Sorted array (Insertion Sort): ";
+    printArray<int>(arr2);
 }
